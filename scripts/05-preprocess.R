a73aab266da7eb3c5f11aa150a78b6a20c8cefe7
hanzholahs <hanzholahs@gmail.com>
2023-12-19 18:19:54 +0700

    Update code

--- scripts/05-preprocess.R
+++ scripts/05-preprocess.R
@@ -2,6 +2,11 @@
 # 
 # Source: https://www.kaggle.com/datasets/uciml/glass
 
+# add section --> ctrl + shift + R
+# comment/uncomment --> ctrcl shift c
+# add pipe --> ctrl shift m
+# restart session --> ctrl shift F10
+
 
 
 
@@ -33,6 +38,28 @@ glass <-
 
 purrr::map_dbl(glass, function(x) mean(!is.na(x)))
 
+distinct(glass, Type)
+count(glass, Type)
+
+
+# working with logical vectors
+
+x <- c(FALSE, TRUE, TRUE, TRUE, FALSE, TRUE)
+
+mean(x)
+sum(x)
+
+c(x, 1) # automatic type casting
+c(x, "1")
+
+y <- c(rep("M", 10), rep("F", 15))
+
+y == "M"
+
+y == c("M", "F", "M", "F", "M")
+
+sum(y == "M")
+mean(y == "M")
 
 
 
@@ -53,34 +80,56 @@ glass_test <- testing(glass_split)
 
 # Create Recipe -----------------------------------------------------------
 
+# normalization
+
+x <- c(4,3,3,2,3,1,4,3,3,2,4,5,4,3,5,6,7,5,3,4,5)
+
+mean(x)
+sd(x)
+
+y <- (x - mean(x)) / sd(x)
+
+mean(y)
+sd(y)
+
+
 # Define a recipe
-glass_rec <-
-  recipe(Type ~ ., data = glass_train) |> 
-  step_center(all_predictors()) |> 
-  step_scale(all_predictors())
+
+glass_rec <- recipe(Type ~ ., data = glass_train)
+
+glass_rec <- step_center(glass_rec, RI, Na, Mg, Al, Si, K, Ca, Ba, Fe)
+
+glass_rec <- step_scale(glass_rec, all_predictors())
   
 glass_rec
+
 
 # Train the recipe (it's different than train a model)
 
 trained_rec <- prep(glass_rec, training = glass_train)
+trained_rec
+
+
 
 # Apply pre-processing to data
 
-new_glass_train <- bake(trained_rec, new_data = NULL)
+# new_data = NULL or new_data = glass_train
+new_glass_train <- bake(trained_rec, new_data = NULL) 
+
 new_glass_test <- bake(trained_rec, new_data = glass_test)
 
 new_glass_train
 new_glass_test
 
+
 # Check the pre-processed data
 
 bind_cols("Variable" = names(glass_train[1:9]),
           purrr::map_dfr(select(glass_train, -Type), function(x) {
-            tibble(old_mean = mean(x), old_stdev = sd(x))
+            tibble(OLD_mean = mean(x), OLD_stdev = sd(x))
           }),
           purrr::map_dfr(select(new_glass_train, -Type), function(x) {
-            tibble(mean = mean(x), stdev = sd(x))
+            tibble(NEW_mean = mean(x), NEW_stdev = sd(x))
           })) |> 
   select(Variable, ends_with("mean"), ends_with("stdev")) |> 
   mutate_if(is.numeric, round, digits = 2)
@@ -91,7 +140,7 @@ bind_cols("Variable" = names(glass_train[1:9]),
 
 # Train a Model -----------------------------------------------------------
 
-# With pre-processed data
+# With pre-processed data (define > fit > predict)
 
 multinom_model <- multinom_reg(mode = "classification")
 
@@ -102,7 +151,7 @@ preds_preprocessed <-
   rename(pred_class_1 = .pred_class)
 
 
-# Without original data
+# With original data
 
 preds_original <- 
   multinom_reg(mode = "classification") |> 
